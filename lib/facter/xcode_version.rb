09c4dd2aa9fd75d0bd3dc9311ea52b9674d3c83f
Sean Abraham <sean.abraham@airbnb.com>
2017-09-13 17:11:50 -0700

    Fix typo

--- lib/facter/xcode_version.rb
+++ lib/facter/xcode_version.rb
@@ -6,7 +6,7 @@ Facter.add(:xcode_version) do
   confine kernel: 'Darwin'
   setcode do
     version = '0'
-    if File.exist?(crypt_info_plist)
+    if File.exist?(xcode_version_plist)
       plist = Puppet::Util::Plist.read_plist_file(xcode_version_plist)
       if plist.include? 'CFBundleShortVersionString'
         version = plist['CFBundleShortVersionString']
