7250e4154d45247a72fbe3e7e4a695cf5501179a
Graham Gilbert <graham@grahamgilbert.com>
2017-10-19 14:47:19 -0700

    Needs more linting when I get time

--- lib/facter/xcode_tools_present.rb
+++ lib/facter/xcode_tools_present.rb
@@ -1,7 +1,15 @@
 Facter.add(:xcode_tools_present) do
   confine kernel: 'Darwin'
   setcode do
+    _ = Facter::Util::Resolution.exec('/usr/sbin/pkgutil --pkgs=com.apple.pkg.CLTools_Executables')
+    if $CHILD_STATUS.exitstatus.zero?
+      receipt_present = true
+    else
+      receipt_present = false
+    end
     if Facter.value(:xcode_active_directory).nil?
+      false
+    elsif receipt_present == false
       false
     else
       true
