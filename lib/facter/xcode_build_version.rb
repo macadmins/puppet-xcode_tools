a8dfccddff970e1ba4912bd1107da301b4b730cb
Steven Hepting <steven.hepting@airbnb.com>
2021-08-25 08:25:10 -0600

    Add Xcode build version fact.

---
+++ lib/facter/xcode_build_version.rb
@@ -0,0 +1,17 @@
+require 'puppet/util/plist' if Puppet.features.cfpropertylist?
+
+xcode_version_plist = '/Applications/Xcode.app/Contents/version.plist'
+
+Facter.add(:xcode_build_version) do
+  confine kernel: 'Darwin'
+  setcode do
+    version = '0000'
+    if File.exist?(xcode_version_plist)
+      plist = Puppet::Util::Plist.read_plist_file(xcode_version_plist)
+      if plist.include? 'ProductBuildVersion'
+        version = plist['ProductBuildVersion']
+      end
+    end
+    version
+  end
+end
